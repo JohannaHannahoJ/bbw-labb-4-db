dd73bdc1b19b321244ff8a7f5e605ea621c6195f
JohannaHannahoJ <joha2102@student.miun.se>
2026-05-05 20:54:19 +0200

    Create route for user registration

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,44 @@
 // Routes för att skapa användare och logga in
 
 const express = require("express");
+const bcrypt = require("bcrypt");
 const router = express.Router();
 const client = require("../db");
 
-// routes kommer
+// routes
+// Lägg till användare
+router.post("/register", async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        if (!username || !password) {
+            return res.status(400).json({ error: "Invalid input" });
+        }
+
+        // kolla om user finns
+        const result = await client.query(
+            "SELECT * FROM users WHERE username = $1", [username]
+        );
+
+        // om användare med samma namn finns, kryptiskt felmeddelande
+        if (result.rows.length > 0) {
+            return res.status(409).json({ error: "Registration failed" });
+        }
+
+        // hash password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // skapa user
+        await client.query(
+            "INSERT INTO users (username, password) VALUES ($1, $2)", [username, hashedPassword]
+        );
+
+        res.status(201).json({ message: "User created" });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server error" });
+    }
+});
 
 module.exports = router;
