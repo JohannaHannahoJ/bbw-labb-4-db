dd2483240be2b646d7a0948932a1e6a53a5dac00
JohannaHannahoJ <joha2102@student.miun.se>
2026-05-06 13:42:52 +0200

    Create user login route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -41,4 +41,44 @@ router.post("/register", async (req, res) => {
     }
 });
 
+// Logga in användare
+router.post("/login", async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        // validera input
+        if (!username || !password) {
+            return res.status(400).json({ error: "Invalid input, send username and password" });
+        }
+
+        // hämta user
+        const result = await client.query(
+            "SELECT * FROM users WHERE username = $1", [username]
+        );
+
+        // om användare inte finns
+        if (result.rows.length === 0) {
+            return res.status(401).json({ message: "Incorrect username/password" });
+        }
+
+        // spara matchande användaren från db
+        const user = result.rows[0];
+
+        // jämför inmatat lösenord med hashat lösenord från db
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        // om password inte stämmer, felmeddelande och hoppa ur funktionen
+        if (!passwordMatch) {
+            return res.status(401).json({ message: "Incorrect username/password" });
+        }
+
+        // korrekt login
+        res.status(200).json({ message: "Correct login!" });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server error" });
+    }
+});
+
 module.exports = router;
